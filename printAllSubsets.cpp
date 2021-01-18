a1a96e34287285cfed0c6f6ea3b834867a6f1f98
PAWAN KUMAR <47324615+Pawan1510@users.noreply.github.com>
2021-01-19 00:52:55 +0530

    Create printAllSubsets.cpp

---
+++ printAllSubsets.cpp
@@ -0,0 +1,43 @@
+//Print All Subsets of a given array
+#include<bits/stdc++.h>
+using namespace std;
+void subsetsUtil( vector<int>& A, vector< vector<int> >& res, vector<int>&subset, int index )
+{
+    res.push_back( subset );
+
+    for( int i=index; i<A.size(); i++ ) {
+
+        subset.push_back( A[i] );
+
+        subsetsUtil( A, res, subset, i+1 );
+
+        subset.pop_back();
+    }
+    return;
+}
+vector< vector<int> > subsets( vector<int>& A )
+{
+    vector<int> subset;
+
+    vector< vector<int> > res;
+
+    int index = 0;
+
+    subsetsUtil( A, res, subset, index );
+
+    return res;
+}
+int main()
+{
+    vector<int> A = { 1, 2, 3 };
+
+    vector< vector<int> > res = subsets( A );
+
+    for( int i=0; i<res.size(); i++ ) {
+        cout<<"{ ";
+        for( int  j=0; j<res[i].size(); j++ ) {
+            cout<<res[i][j]<<" ";
+        }
+        cout<<"}"<<endl;
+    }
+}
